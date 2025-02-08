4cc6d903a81c3acaf5291e0c629130713c3b0215
Mara <mara-li@outlook.fr>
2025-02-08 10:39:01 +0100

    test: add another cases in the content

--- tests/parse_file.test.ts
+++ tests/parse_file.test.ts
@@ -2,18 +2,7 @@ import { describe, expect, it } from "bun:test";
 import type { Thesaurus } from "../src/interfaces";
 import { getTags } from "../src/utils";
 
-describe("getTags", () => {
-	it("should return an empty array if no tags are found", () => {
-		const content = "This is a test content without any tags.";
-		const thesaurus: Thesaurus = {
-			tag1: new Set(["synonym1", "synonym2"]),
-			tag2: new Set(["synonym3", "synonym4"]),
-		};
-
-		const result = getTags(content, thesaurus);
-		expect(result).toEqual([]);
-	});
-
+describe("should return tags", () => {
 	it("should return tags if synonyms are found in the content", () => {
 		const content = "This content contains synonym1 and synonym4.";
 		const thesaurus: Thesaurus = {
@@ -35,7 +24,29 @@ describe("getTags", () => {
 		const result = getTags(content, thesaurus);
 		expect(result).toEqual(["tag1", "tag2"]);
 	});
+	it("should return the two tags related to the synonyms", () => {
+		const content = "This content contains synonym4.";
+		const thesaurus: Thesaurus = {
+			tag1: new Set(["synonym1", "synonym4"]),
+			tag2: new Set(["synonym3", "synonym4"]),
+		};
 
+		const result = getTags(content, thesaurus);
+		expect(result).toEqual(["tag1", "tag2"]);
+	});
+
+	it("should return the tags if the synonyms is equal to the tag", () => {
+		const content = "This content contains tag1.";
+		const thesaurus: Thesaurus = {
+			tag1: new Set(["tag1", "synonym1"]),
+			tag2: new Set(["synonym3", "synonym4"]),
+		};
+
+		const result = getTags(content, thesaurus);
+		expect(result).toEqual(["tag1"]);
+	});
+});
+describe("should not return tags", () => {
 	it("should not return duplicate tags", () => {
 		const content = "This content contains synonym1 and synonym1 again.";
 		const thesaurus: Thesaurus = {
@@ -46,15 +57,24 @@ describe("getTags", () => {
 		const result = getTags(content, thesaurus);
 		expect(result).toEqual(["tag1"]);
 	});
-
-	it("should return the two tags related to the synonyms", () => {
-		const content = "This content contains synonym4.";
+	it("should return an empty array if no tags are found", () => {
+		const content = "This is a test content without any tags.";
 		const thesaurus: Thesaurus = {
-			tag1: new Set(["synonym1", "synonym4"]),
+			tag1: new Set(["synonym1", "synonym2"]),
 			tag2: new Set(["synonym3", "synonym4"]),
 		};
 
 		const result = getTags(content, thesaurus);
-		expect(result).toEqual(["tag1", "tag2"]);
+		expect(result).toEqual([]);
+	});
+	it("should not return tags if the synonyms is in a word", () => {
+		const content = "This content contains synonym1word.";
+		const thesaurus: Thesaurus = {
+			tag1: new Set(["synonym1", "synonym2"]),
+			tag2: new Set(["synonym3", "synonym4"]),
+		};
+
+		const result = getTags(content, thesaurus);
+		expect(result).toEqual([]);
 	});
 });
